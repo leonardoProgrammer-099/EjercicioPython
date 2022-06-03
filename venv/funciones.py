fddabb7f956253e16120354877d74fe87337216d
leogarcia99 <darkargarcia85@gmail.com>
2022-06-02 19:09:25 -0500

    funciones

---
+++ venv/funciones.py
@@ -0,0 +1,7 @@
+def areaTriangulo(b ,a):
+    print("la base del triangulo es" , (b*a))
+def areaCirculo(r):
+    print("El area del circulo es ", (3.1426 * r))
+
+areaTriangulo(2,3)
+areaCirculo(4)
