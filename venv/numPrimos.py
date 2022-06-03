1a64bc741e944d781d99b5f7a0b50f74a10eea12
leogarcia99 <darkargarcia85@gmail.com>
2022-06-02 19:18:03 -0500

    numeros primos

---
+++ venv/numPrimos.py
@@ -0,0 +1,10 @@
+def esPrimo(x):
+    if(x <0):
+        print ("El numero debe ser positivo")
+    else:
+        if(x != 0):
+            print ("el numero es primo")
+        else:
+            print ("el numero es entero")
+
+esPrimo(2)
